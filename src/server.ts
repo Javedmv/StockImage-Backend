58002b02eb8576e02cffade671588e137c2ef818
Javedmv <javedmv777@gmail.com>
2025-06-05 15:02:22 +0530

    corrected port

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ dotenv.config();
 
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT || 5000);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port:${PORT}`);
   connectDB();
 });
